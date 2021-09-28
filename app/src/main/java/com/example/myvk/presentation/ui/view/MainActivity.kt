8389a604b856a59e7929cefc231d15e8e8b5e3d3
Igor Novikov <inovikov@rit-it.com>
2021-09-28 13:50:23 +0700

    заменена функция замены фрагмента на отображение фрагмента

--- app/src/main/java/com/example/myvk/presentation/ui/view/MainActivity.kt
+++ app/src/main/java/com/example/myvk/presentation/ui/view/MainActivity.kt
@@ -1,24 +1,21 @@
 package com.example.myvk.presentation.ui.view
 
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.myvk.R
-import com.example.myvk.domain.model.FriendModel
-import com.example.myvk.domain.use_case.VKFriendsRequest
+import com.example.myvk.databinding.ActivityLoginBinding
+import com.example.myvk.databinding.ActivityMainBinding
 import com.example.myvk.presentation.ui.view.fragment.Friends.FriendsFragment
 import com.example.myvk.presentation.ui.view.fragment.GroupsFragment
 import com.example.myvk.presentation.ui.view.fragment.NewsFragment
 import com.example.myvk.presentation.ui.view.fragment.Friends.FriendsViewModel
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.vk.api.sdk.*
-import com.vk.api.sdk.auth.VKAccessToken
-import com.vk.api.sdk.auth.VKAuthCallback
-import com.vk.api.sdk.auth.VKScope
+
+
+
 
 
 class MainActivity : AppCompatActivity() {
@@ -26,36 +23,50 @@ class MainActivity : AppCompatActivity() {
 
     var friendsViewModel: FriendsViewModel? = null
 
+    var fragmentList: MutableList<Fragment> = ArrayList()
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         friendsViewModel = ViewModelProvider(this).get(FriendsViewModel::class.java)
-
-
 
         val newsFragment = NewsFragment()
         val friendsFragment = FriendsFragment()
         val groupsFragment = GroupsFragment()
 
-        makeCurrentFragment(newsFragment)
 
-        findViewById<BottomNavigationView>(R.id.bottom_navigation).setOnNavigationItemSelectedListener {
+        supportFragmentManager.beginTransaction().apply {
+            add(R.id.fl_wrapper, newsFragment)
+            add(R.id.fl_wrapper, friendsFragment)
+            add(R.id.fl_wrapper, groupsFragment)
+            fragmentList.add(newsFragment)
+            fragmentList.add(friendsFragment)
+            fragmentList.add(groupsFragment)
+            commit()
+        }
+
+        displayFragment(newsFragment)
+
+        findViewById<BottomNavigationView>(binding.bottomNavigation.id).setOnNavigationItemSelectedListener {
             when (it.itemId) {
-                R.id.ic_news -> makeCurrentFragment(newsFragment)
-                R.id.ic_friends -> makeCurrentFragment(friendsFragment)
-                R.id.ic_groups -> makeCurrentFragment(groupsFragment)
+                R.id.ic_news -> displayFragment(newsFragment)
+                R.id.ic_friends -> displayFragment(friendsFragment)
+                R.id.ic_groups -> displayFragment(groupsFragment)
             }
             true
         }
     }
 
-    private fun makeCurrentFragment(fragment: Fragment) {
+    private fun displayFragment(fragment: Fragment) {
         supportFragmentManager.beginTransaction().apply {
-            replace(R.id.fl_wrapper, fragment)
+            for (i in fragmentList.indices)
+                hide(fragmentList[i])
+            show(fragment)
             commit()
         }
     }
-
-
 }
